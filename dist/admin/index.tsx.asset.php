35d785505780e4fb054bd9e58f7d30227a61c829
Carl Youngman <me@carlyoungman.com>
2025-12-21 22:23:27 +0000

    updated the information within info card

--- dist/admin/index.tsx.asset.php
+++ dist/admin/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => 'f71852655ec99ca91aba');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => 'ace620e960417465e643');
--- src/admin/components/InfoCard.tsx
+++ src/admin/components/InfoCard.tsx
@@ -2,21 +2,35 @@ export default function InfoCard() {
   return (
     <section className="dmn-admin__card">
       <h2>Information</h2>
-      <p>To display the booking widget you can use the following shortcode.</p>
-      <h6>Options</h6>
+
+      <p>
+        Use the shortcode below to embed the booking widget on any page or post. You can optionally
+        pass parameters to preselect venues, activities, or limit available booking days.
+      </p>
+
+      <p>Shortcode options</p>
       <ul>
-        <li>
-          Preselect a venue by including a <b>venue_id</b> on the shortcode.
+        <li style={{ marginBottom: '0.75rem' }}>
+          <b>venue_id</b> – Preselect a specific venue. You can use "inherit" to inherit the venue
+          id from the location settings
         </li>
-        <li>
-          Preselect an activity by including a <b>type_id</b> on the shortcode. A <b>venue_id</b> is
-          required.
+
+        <li style={{ marginBottom: '0.75rem' }}>
+          <b>type_id</b> – Preselect one or more activity types.
+          <br />
+          Use a comma-separated list for multiple types.
+          <br />
+          <i>Note: venue_id is required when using type_id.</i>
         </li>
+
         <li>
-          Preselect days of the week by including a <b>allowed_days</b> on the shortcode. List the
-          days in a comma separated list
+          <b>allowed_days</b> – Restrict bookings to specific days of the week.
+          <br />
+          Use a comma-separated list (e.g. Monday,Friday,Saturday).
         </li>
       </ul>
+
+      <p>Example shortcode:</p>
       <code>[dmn_booking venue_id="" type_id="" allowed_days=""]</code>
     </section>
   );
