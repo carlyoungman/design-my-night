516f87e4ff033bc80d9dfe0a86d1193bef143756
Carl Youngman <me@carlyoungman.com>
2026-03-06 11:48:17 +0000

    Added a set width to the group size input to fix a firefox mac bug

--- dist/admin/index.tsx.asset.php
+++ dist/admin/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '022e287cfe325762e2d7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '6287bfc308c3b5fe5021');
--- dist/frontend/index.tsx.asset.php
+++ dist/frontend/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '4812c18e635843e46b0a');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '8447e69a82ff9e212ea2');
