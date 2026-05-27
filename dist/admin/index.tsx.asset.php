46f31ac1638b8a70a1d6569ef9325d0c87bb9f1a
Carl Youngman <me@carlyoungman.com>
2026-05-27 23:48:12 +0100

    latest

--- dist/admin/index.tsx.asset.php
+++ dist/admin/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => 'f35e141a05ace406840c');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '865463e626e9a3752d10');
